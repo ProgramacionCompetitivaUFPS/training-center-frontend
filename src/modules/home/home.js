8982452d4949fcc94a2f26984b354cc2fef8fd8b
Crisel Ayala <criseljazminal@ufps.edu.co>
2021-09-15 18:08:34 -0500

    Se relaciona el modal de inicio de sesión y registro desde la vista home.

--- src/modules/home/home.js
+++ src/modules/home/home.js
@@ -1,13 +1,32 @@
-import Swiper from 'swiper/swiper-bundle.min.js';
+import Swiper from 'swiper/swiper-bundle.min.js'
+import { inject } from 'aurelia-framework'
+import { Router } from 'aurelia-router'
+import { Auth } from 'services/services'
 
+/**
+ * Home (Module)
+ * Clase que genera la vista bienvenida.
+ * @export
+ * @class Home
+ */
 
+@inject(Auth, Router)
 export class Home {
     
-    constructor() {
-        var swiper = new Swiper('.mySwiper', {
+  
+    /**
+     * Crea una instancia de Home.
+     * @param {service} authService - Servicio de autenticación
+     * @param {service} routerService - Servicio de enrutamiento
+     */
+    constructor(authService, routerService) {
+      this.authService = authService
+      this.routerService = routerService
+
+        var swiper = new Swiper(this.mySwiper, {
             pagination: {
-              el: '.swiper-pagination',
+              el: this.swiperPagination,
             },
-          });
+          })
     }
 }
