d3ef6b1b935ca831a8cb112f8ce8529e4d0b57c3
GenesisDaniela <genesisdanielavjau@ufps.edu.co>
2022-06-29 22:39:01 -0500

    Update enviroment.js

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,6 +1,6 @@
 export default {
     debug: true,
     testing: true,
-    API_BACKEND_URL_DEVELOPMENT: "http://146.148.49.88:9191/api"
+    API_BACKEND_URL_DEVELOPMENT: "http://146.148.49.88:9191/api/"
         //  API_BACKEND_URL_DEVELOPMENT: "http://localhost:3000/api/"
 };
