fbe3c521f17ec371b5f31c295fad203454a5c91b
cryselayala98 <criseljazminal@ufps.edu.co>
2022-11-15 14:24:56 -0500

    Ajuste mejoras visualizacion de problemas y foro con disqus

--- src/resources/elements/disqus.js
+++ src/resources/elements/disqus.js
@@ -1,12 +1,14 @@
-import {bindable, customElement} from 'aurelia-framework';
+import {bindable, customElement} from 'aurelia-framework'  
   
   @customElement('disqus')
   export class Disqus {
   
-    @bindable post;
+    @bindable post 
   
     bind(bindingContext) {
-      this.post = bindingContext.post;
+      console.log("this.possssssst",this.post)
+      console.log("this.possssssst",bindingContext.post)
+      this.post = bindingContext.post
     }
   
     attached() { 
@@ -14,11 +16,13 @@ import {bindable, customElement} from 'aurelia-framework';
       DISQUS.reset({ 
         reload: true,
         config: function() {
-          this.page.identifier = this.post.identifier;
-          //this.page.url = process.env.URL_DISCUSS + this.post.subdirectory;
-          this.page.title = newPost.title;
-          console.log("this.page.url", this.page.url)
+          console.log("disqussssssssssssssssss")
+          console.log("this.page.url", this.post)
+          this.page.identifier = this.post
+          //this.page.url = process.env.URL_DISCUSS + this.post.subdirectory
+          this.page.title = newPost.title
+          this.language = "es"
         }
-      });
+      }) 
     }
   }
