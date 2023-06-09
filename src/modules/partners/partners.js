6928352b7087373a24b82e82c081b75537560b1e
Santiago Serrano <55068431+GenesisDaniela@users.noreply.github.com>
2023-06-09 01:47:25 -0500

    parterns section

--- src/modules/partners/partners.js
+++ src/modules/partners/partners.js
@@ -3,12 +3,13 @@ const importedData = require("../../assets/json/partners.json");
 export class Partners {
 
     col;
-
+    activated=true;
     constructor() {
         this.getJson();
     }
 
     getJson() {
         this.col = importedData;
+        
     }
 }
