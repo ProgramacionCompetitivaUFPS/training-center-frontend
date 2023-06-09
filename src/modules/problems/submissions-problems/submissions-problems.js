db24d2b37dc96d9e88ee1b82fbec3be712d0a347
Santiago Serrano <55068431+GenesisDaniela@users.noreply.github.com>
2023-06-09 14:03:39 -0500

    submissions problems done

--- src/modules/problems/submissions-problems/submissions-problems.js
+++ src/modules/problems/submissions-problems/submissions-problems.js
@@ -2,9 +2,11 @@ import { inject, observable } from 'aurelia-framework'
 import { Router } from 'aurelia-router'
 
 import { MESSAGES } from 'config/config'
-import { Category, Problem } from 'models/models'
 import { Alert, Auth, Problems } from 'services/services'
-
+import {
+  Material,
+  Enums
+} from 'models/models'
 /**
  * SubmissionsProblems (Module)
  * Módulo encargado de mostrar los problemas que hacen parte de una categoría
@@ -31,7 +33,7 @@ export class SubmissionsProblems {
    constructor(alertService, authService, problemService, routerService) {
        this.alertService = alertService
        this.authService = authService
-       this.problemsService = problemService
+       this.problemService = problemService
        this.routerService = routerService
        this.totalPages = 1
        this.page = 1
@@ -44,12 +46,42 @@ export class SubmissionsProblems {
        this.pagination = []
        this.language = 'Cualquier idioma'
        this.title=""
+       
+    this.isABlocklyCode = false
+    this.veredictOptions = [{
+        value: 'ALL',
+        text: 'Cualquier veredicto'
+      },
+      {
+        value: 'ACC',
+        text: 'Correcto'
+      },
+      {
+        value: 'TL',
+        text: 'Tiempo límite excedido'
+      },
+      {
+        value: 'WA',
+        text: 'Respuesta incorrecta'
+      },
+      {
+        value: 'RT',
+        text: 'Error en tiempo de ejecución'
+      },
+      {
+        value: 'CE',
+        text: 'Error de compilación'
+      }
+    ]
+    this.veredict = this.veredictOptions[0]
+    this.downloadMesagge = 'Descargar código'
+    this.enums = Enums
+    this.logsMessagge = 'No hay mensajes para mostrar'
    }
 
    viewCode(submission) {
     this.downloadActive = false
     this.submissionLoaded = submission
-    console.log(submission,"XXXX")
     this.submissionLoaded.code = 'Cargando código...'
     window.$('#submission-detail').modal('show')
     this.problemService.getSubmission(this.submissionLoaded.file_name)
@@ -80,8 +112,42 @@ export class SubmissionsProblems {
           this.alertService.showMessage(MESSAGES.unknownError)
         }
       }) 
+      this.viewLogs()
+      this.showLogs(0)
   }
+  viewLogs() {
+    this.logsMessagge = 'No hay mensajes para mostrar'
+    let errorExtension = ''
+    if (this.submissionLoaded.verdict == 'Compilation Error') errorExtension = '.out'
+    else if (this.submissionLoaded.verdict == 'Runtime Error') errorExtension = '.err'
+    if (errorExtension == '') return
 
+    this.problemService.getSubmissionLog(this.submissionLoaded.file_name.split('.')[0] + errorExtension)
+      .then(data => {
+        let reader = new FileReader()
+        reader.onload = () => {
+          if (reader.result != undefined)
+            this.logsMessagge = reader.result
+        }
+        reader.readAsText(data)
+      })
+      .catch(error => {
+        if (error.status === 401 || error.status === 403)
+          this.alertService.showMessage(MESSAGES.permissionsError)
+        else if (error.status === 500)
+          this.alertService.showMessage(MESSAGES.serverError)
+        else
+          this.alertService.showMessage(MESSAGES.unknownError)
+      })
+  }
+  showLogs(bit) {
+    document.getElementById("logs-detail").textContent = this.logsMessagge;
+    document.getElementById("code-detail").style.display = bit ? "none" : "block"
+    document.getElementById("view-logs-btn").style.display = bit ? "none" : "block"
+    document.getElementById("view-code-btn").style.display = bit ? "block" : "none"
+    document.getElementById("code-download-btn").style.display = bit ? "none" : "block"
+    document.getElementById("log-detail").style.display = bit ? "block" : "none"
+  }
   viewSvgSubmission(submission){
     this.problemService.getSvgSubmission(this.submissionLoaded.blockly_file_name)
       .then(data => {
@@ -156,8 +222,9 @@ export class SubmissionsProblems {
        else if (this.sort === 'Lenguaje') stringSort = 'language'
        if (this.language === 'Español') stringLang = 'es'
        else if (this.language === 'Inglés') stringLang = 'en'
-       this.problemsService.getSubmissions(this.id, this.page, this.limit, stringSort, (this.by === 'Ascendente' ? 'asc' : 'desc'), stringLang)
+       this.problemService.getSubmissions(this.id, this.page, this.limit, stringSort, (this.by === 'Ascendente' ? 'asc' : 'desc'), stringLang)
             .then(data => {
+                console.log(data,"XXXXXXXx")
                 this.title=data.meta.title
                 this.totalPages = data.meta.totalPages
                 if (this.totalPages !== 0) {
